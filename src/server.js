ac33ca945473732507a661454da3152f3cc8470b
Matheus Amon <amonmatheus757@gmail.com>
2023-04-24 16:42:14 -0300

    Adicioando Model e Controller de produtos

--- src/server.js
+++ src/server.js
@@ -1,13 +1,13 @@
 const express = require('express')
 //const path = require('path')
 
-//const db = require('./database')
+const db = require('./database/db')
 const routes = require('./routes/routes')
 
 const app = express()
 
 //conexão com o db
-//db.connect()
+db.connect()
 
 //habilitando server para recebimento de dados via post formulário
 app.use(express.urlencoded({extended: true}))
